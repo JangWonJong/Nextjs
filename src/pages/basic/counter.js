a06281789c943bbc078d07d823bcb7b5a91f120e
JangWonJong <jwj9922@naver.com>
2022-03-30 19:49:00 +0900

    2022-03-30

--- src/pages/basic/counter.js
+++ src/pages/basic/counter.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
+import style from "styles/basic/counter.module.css"
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
+  const count = useSelector(selectCount)
   const dispatch = useDispatch()
+  const [incrementAmount, setIncrementAmount] = useState('0')
+  const incrementValue = Number(incrementAmount) || 0
 
 
   return (<>
---
+++ src/store/index.js
@@ -0,0 +1,2 @@
+import { configureStore } from "@reduxjs/toolkit"
+import reducer from 'store/modules/basic/counterSlice'
